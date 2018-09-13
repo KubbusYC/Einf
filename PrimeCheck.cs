7c5f7bf812825e901bd2f5e6c791c2a0a194538d
KubbusYC <32298129+KubbusYC@users.noreply.github.com>
2018-09-13 11:28:21 +0200

    Create PrimeCheck.cs

---
+++ PrimeCheck.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCheck
+{
+
+    class Program
+    {
+        
+
+        
+
+        
+
+
+
+        static void Main(string[] args)
+        {
+
+            //Function Definitions
+              bool checkPrimes (int a, List<int> oldPrimes) 
+            {
+                int temp = 0;
+                bool isPrime = true;
+                int root = (int)(Math.Round(Math.Sqrt(a), 0));
+                while (isPrime && temp < oldPrimes.Count && oldPrimes[temp] <= root)
+                {
+                    if(Divisible(a, oldPrimes[temp]))
+                    {
+                        isPrime = false;
+                        temp++;
+                    }
+                    else { temp++; }
+                }
+                return isPrime;
+            }//checks if a is not devidable by all of oldPrimes
+            bool Divisible(int a, int b)
+            {
+                if (a % b == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            } //checks if a is divisible by b
+              int  nextPrime(List<int> oldPrimes)
+            {
+                int last = oldPrimes[oldPrimes.Count - 1];
+                while (true)
+                {
+                    last++;
+                    if (checkPrimes(last, oldPrimes))
+                    { return last; }
+
+                }
+
+            }//adds the next prime to old Primes
+
+            //Variable Declaration
+            int n;
+            bool fail = false;
+            List<int> primes = new List<int>();
+            primes.Add(2);
+
+          
+
+            Console.WriteLine("Number:");
+            if (int.TryParse(Console.ReadLine(),out n))     //Read input
+            {
+                while(!fail && primes[primes.Count-1] <= (int)(Math.Round(Math.Sqrt(n), 0)))
+                {
+                    if(!checkPrimes(n, primes))
+                    {
+                        fail = true;
+                    }
+                    else
+                    {
+                        primes.Add(nextPrime(primes));
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            Console.WriteLine((!fail).ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
